4a1dce394d19d5d2965733b910cec3f298d392dc
Praytic <chernogorovvm@gmail.com>
2021-01-14 09:55:06 +0300

    Change file loading mechnism in DatastoreConfig

--- src/main/java/org/praytic/discord/statsbot/config/DatastoreConfig.java
+++ src/main/java/org/praytic/discord/statsbot/config/DatastoreConfig.java
@@ -5,6 +5,7 @@ import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.praytic.discord.statsbot.config.properties.GoogleProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +13,10 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @RequiredArgsConstructor
+@Slf4j
 @Configuration
 @EnableConfigurationProperties(GoogleProperties.class)
 public class DatastoreConfig {
@@ -22,10 +25,13 @@ public class DatastoreConfig {
 
     @Bean
     public Datastore datastore() throws IOException {
+        InputStream googleCreds = ClassLoader.getSystemResourceAsStream(googleProperties.getApplicationCredentials());
+        if (googleCreds == null) {
+            throw new RuntimeException("Google application credentials file does not exist or the path is incorrect.");
+        }
         return DatastoreOptions.newBuilder()
                 .setProjectId(googleProperties.getProjectId())
-                .setCredentials(ServiceAccountCredentials.fromStream(
-                        new FileInputStream(googleProperties.getApplicationCredentials())))
+                .setCredentials(ServiceAccountCredentials.fromStream(googleCreds))
                 .build()
                 .getService();
     }
